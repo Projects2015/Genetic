48a8cef03a1713d120cd87474bdce5b42932e024
Josh <Josh@nat-oitwireless-inside-vapornet100-d-5256.princeton.edu>
2015-03-14 16:52:35 -0400

    blah

--- Genetic/src/Creature.java
+++ Genetic/src/Creature.java
@@ -1,6 +1,10 @@
 
 public class Creature {
     
+    private double x;
+    private double y;
+    
+    
     public static void main(String[] args) {
         System.out.println("Blah Blah BlahBlah");
     }
