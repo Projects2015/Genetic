0ac53464c720db23d9ba00a0e66c4ae1e6404c09
Daniel <Daniel@Stanley5.com>
2015-03-14 16:50:45 -0400

    Created environment

---
+++ Genetic/src/Environment.java
@@ -0,0 +1,16 @@
+public class Environment {
+    private static final int GENERATIONS = 100;
+    private static int MAX_CREATURES = 10;
+
+    public static void main(String[] args) {
+        Creature[] creatures = new Creature[MAX_CREATURES];
+        for (Creature creature : creatures) {
+            creature = new Creature();
+        }
+
+        for (int i = 0; i < GENERATIONS; i++) {
+            
+        }
+    }
+
+}
